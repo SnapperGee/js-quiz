caf2d3be08ebdc5f75c08353b19148f0afa1af90
Snap <XxSnapperGeexX@gmail.com>
2023-08-22 18:06:51 -0700

    Implement IterableIterator<boolean> in JsQuiz class as place holder.

--- src/main/ts/js-quiz.ts
+++ src/main/ts/js-quiz.ts
@@ -1,4 +1,23 @@
-export class JsQuiz
+export class JsQuiz implements IterableIterator<boolean>
 {
+    readonly #arr: readonly boolean[] = [];
+    #iterIndex: number = 0;
 
+    public next(): IteratorResult<boolean>
+    {
+        if (this.#iterIndex < this.#arr.length)
+        {
+            return { done: false, value: this.#arr[this.#iterIndex++] };
+        }
+        else
+        {
+            this.#iterIndex = 0; // reset the pointer to start for new iterations
+            return { done: true, value: null };
+        }
+    }
+
+    public [Symbol.iterator](): IterableIterator<boolean>
+    {
+        return this;
+    }
 }
