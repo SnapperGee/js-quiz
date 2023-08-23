8c73c6c0336468a750aecd2253cfb566dd36f78b
Snap <XxSnapperGeexX@gmail.com>
2023-08-22 22:52:29 -0700

    Add correctAnswers and hasMultipleCorrect properties to Qeustion class and add getters for all properties.

--- src/main/ts/question.ts
+++ src/main/ts/question.ts
@@ -4,6 +4,9 @@ export class Question
 {
     readonly #prompt: string;
     readonly #answers: readonly Readonly<Answer>[];
+    readonly #correctAnswers: readonly Readonly<Answer>[];
+    readonly #hasMultipleCorrect: boolean;
+    readonly #string: string;
 
     public constructor(prompt: NonNullable<string>, answers: NonNullable<readonly Answer[]>)
     {
@@ -19,5 +22,15 @@ export class Question
 
         this.#prompt = prompt;
         this.#answers = Object.freeze(answers.map(answer => Object.isFrozen(answer) ? answer : Object.freeze(createAnswer(answer))));
+        this.#correctAnswers = Object.freeze(this.#answers.filter(answer => answer.isCorrect));
+        this.#hasMultipleCorrect = this.#correctAnswers.length !== 1;
+        this.#string = `${new.target.name} {prompt: "${this.#prompt}", answers: [${this.#answers.map(answer => `{answer: "${answer.answer}", isCorrect: ${answer.isCorrect}}`).join(", ")}]}`;
     }
+
+    public get prompt(): string { return this.#prompt; }
+    public get answers(): readonly Readonly<Answer>[] { return this.#answers; }
+    public get correctAnswers(): readonly Readonly<Answer>[] { return this.#correctAnswers; }
+    public get hasMultipleCorrect(): boolean { return this.#hasMultipleCorrect; }
+
+    public toString(): string { return this.#string; }
 }
