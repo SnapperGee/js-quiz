14861bd8dbcc6808e825b919a446462edfd5a7cb
Snap <XxSnapperGeexX@gmail.com>
2023-08-24 15:43:06 -0700

    Init Questions module.

---
+++ src/main/ts/questions.ts
@@ -0,0 +1,51 @@
+import { answer } from "./quiz/answer.js";
+import { Question } from "./quiz/question.js";
+
+export const betterThanJSQuestion: Question = new Question(
+    "What language is better than JavaScript?",
+    [
+        answer("Python", true),
+        answer("TypeScript", true),
+        answer("C#", true),
+        answer("Java", true)
+    ]
+);
+
+export const builtinJSTypesQuestion: Question = new Question(
+    "What is not a builtin type of JavaScript?",
+    [
+        answer("int", true),
+        answer("BigInt", false),
+        answer("string", false),
+        answer("boolean", false)
+    ]
+);
+
+export const jsCreationTimeQuestion: Question = new Question(
+    "How long did it take to create JavaScript?",
+    [
+        answer("10 days", true),
+        answer("Several years", false),
+        answer("6 months", false),
+        answer("A few hours", false)
+    ]
+);
+
+export const jsPrioritiesDuringCreationQuestion: Question = new Question(
+    "When JavaScript was being created, what was it's main priority?",
+    [
+        answer("Developing it ASAP", true),
+        answer("Scalability", false),
+        answer("Longevity", false),
+        answer("Performance", false)
+    ]
+);
+
+export const questions: readonly Question[] = Object.freeze([
+    betterThanJSQuestion,
+    builtinJSTypesQuestion,
+    jsCreationTimeQuestion,
+    jsPrioritiesDuringCreationQuestion
+]);
+
+export default questions;
