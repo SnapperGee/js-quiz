655b61f83b996c3edf024c4dcb44877f127983b6
Snap <XxSnapperGeexX@gmail.com>
2023-08-24 14:16:29 -0700

    Make Question answers and correctAnswers array property elements not Readonly.

--- src/main/ts/quiz/question.ts
+++ src/main/ts/quiz/question.ts
@@ -1,10 +1,10 @@
-import { type Answer, answer as createAnswer} from "./answer.js";
+import { type Answer } from "./answer.js";
 
 export class Question
 {
     readonly #prompt: string;
-    readonly #answers: readonly Readonly<Answer>[];
-    readonly #correctAnswers: readonly Readonly<Answer>[];
+    readonly #answers: readonly Answer[];
+    readonly #correctAnswers: readonly Answer[];
     readonly #hasMultipleCorrect: boolean;
     readonly #string: string;
 
@@ -21,14 +21,14 @@ export class Question
         }
 
         this.#prompt = prompt;
-        this.#answers = Object.freeze(answers.map(answer => Object.isFrozen(answer) ? answer : Object.freeze(createAnswer(answer))));
+        this.#answers = Object.isFrozen(answers) ? answers : Object.freeze(Array.from(answers));
         this.#correctAnswers = Object.freeze(this.#answers.filter(answer => answer.isCorrect));
         this.#hasMultipleCorrect = this.#correctAnswers.length !== 1;
         this.#string = `${new.target.name} {prompt: "${this.#prompt}", answers: [${this.#answers.map(answer => `{answer: "${answer.text}", isCorrect: ${answer.isCorrect}}`).join(", ")}]}`;
     }
 
     public get prompt(): string { return this.#prompt; }
-    public get answers(): readonly Readonly<Answer>[] { return this.#answers; }
+    public get answers(): readonly Answer[] { return this.#answers; }
     public get correctAnswers(): readonly Readonly<Answer>[] { return this.#correctAnswers; }
     public get hasMultipleCorrect(): boolean { return this.#hasMultipleCorrect; }
 
