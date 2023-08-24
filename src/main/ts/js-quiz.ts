46d4c4813e7a3d9c2656b8e226ed0084bc8b410e
Snap <XxSnapperGeexX@gmail.com>
2023-08-24 02:13:42 -0700

    Begin adding question logic.

--- src/main/ts/js-quiz.ts
+++ src/main/ts/js-quiz.ts
@@ -1,7 +1,10 @@
 import { button } from "./dom/button.js";
 import { domElement} from "./dom/dom-element.js";
+import { answer, type Answer } from "./quiz/answer.js";
+import { Question } from "./quiz/question.js";
 
 const paragraphPrompt = domElement(document.querySelector("p"));
+const promptOptionsContainer = domElement(document.querySelectorAll("section")[1]);
 const startButton = button(document.querySelector("button"));
 
 const hideParagraphPromptAndRemoveStartButton = () =>
@@ -11,6 +14,16 @@ const hideParagraphPromptAndRemoveStartButton = () =>
 };
 
 startButton.addClickEventListener(hideParagraphPromptAndRemoveStartButton);
+
+const question1Prompt = "This is the first question";
+const question1AnswerOptions: readonly Answer[] = [
+    answer("Incorrect answer", false),
+    answer("Another incorrect answer", false),
+    answer("CORRECT answer", true),
+    answer("Third incorrect answer", false)
+];
+
+const firstQuestion = new Question(question1Prompt, question1AnswerOptions);
 
 // export class JsQuiz implements IterableIterator<boolean>
 // {
