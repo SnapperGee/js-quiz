4ccec8d7580eb18b3be9929ac38d9b67d04e9f6e
Snap <XxSnapperGeexX@gmail.com>
2023-08-24 11:52:17 -0700

    Begin adding column of answer buttons.

--- src/main/ts/js-quiz.ts
+++ src/main/ts/js-quiz.ts
@@ -1,29 +1,39 @@
-import { button } from "./dom/button.js";
-import { domElement} from "./dom/dom-element.js";
-import { answer, type Answer } from "./quiz/answer.js";
-import { Question } from "./quiz/question.js";
+// import { button } from "./dom/button.js";
+// import { domElement} from "./dom/dom-element.js";
+// import { answer, type Answer } from "./quiz/answer.js";
+// import { Question } from "./quiz/question.js";
 
-const paragraphPrompt = domElement(document.querySelector("p"));
-const promptOptionsContainer = domElement(document.querySelectorAll("section")[1]);
-const startButton = button(document.querySelector("button"));
+// const paragraphPrompt = domElement(document.querySelector("p"));
+// const promptOptionsContainer = domElement(document.querySelectorAll("section")[1]);
+// const startButton = button(document.querySelector("button"));
 
-const hideParagraphPromptAndRemoveStartButton = () =>
-{
-    paragraphPrompt.remove();
-    startButton.remove();
-};
+// const hideParagraphPromptAndRemoveStartButton = () =>
+// {
+//     paragraphPrompt.remove();
+//     startButton.remove();
+// };
 
-startButton.addClickEventListener(hideParagraphPromptAndRemoveStartButton);
+// startButton.addClickEventListener(hideParagraphPromptAndRemoveStartButton);
 
-const question1Prompt = "This is the first question";
-const question1AnswerOptions: readonly Answer[] = [
-    answer("Incorrect answer", false),
-    answer("Another incorrect answer", false),
-    answer("CORRECT answer", true),
-    answer("Third incorrect answer", false)
-];
+// const question1Prompt = "This is the first question";
+// const question1AnswerOptions: readonly Answer[] = [
+//     answer("Incorrect answer", false),
+//     answer("Another incorrect answer", false),
+//     answer("CORRECT answer", true),
+//     answer("Third incorrect answer", false)
+// ];
 
-const firstQuestion = new Question(question1Prompt, question1AnswerOptions);
+// const firstQuestion = new Question(question1Prompt, question1AnswerOptions);
+
+
+
+
+
+
+
+
+
+
 
 // export class JsQuiz implements IterableIterator<boolean>
 // {
