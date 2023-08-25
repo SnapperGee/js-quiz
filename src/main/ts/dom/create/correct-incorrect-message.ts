ff2466307c3e902d0a1ce7c2b7854834c1612722
Snap <XxSnapperGeexX@gmail.com>
2023-08-25 00:04:19 -0700

    Init correct-incorrect-message module.

---
+++ src/main/ts/dom/create/correct-incorrect-message.ts
@@ -0,0 +1,17 @@
+const positiveEmojis: readonly string[] = Object.freeze([
+    "☺", "😀", "😁", "😃", "🙂", "😊", "😌", "🥳", "🎉", "🥂", "🍾", "👍", "👌"
+]);
+
+const negativeEmojis: readonly string[] = Object.freeze([
+    "☹", "😢", "😭", "😠", "😑", "😔", "😕", "😖", "😟", "😣", "😩", "😱", "🤬", "👎"
+]);
+
+export const randomPositiveEmoji = Object.freeze((): string =>
+{
+    return positiveEmojis[Math.floor(Math.random() * positiveEmojis.length)];
+});
+
+export const randomNegativeEmoji = Object.freeze((): string =>
+{
+    return negativeEmojis[Math.floor(Math.random() * negativeEmojis.length)];
+});
